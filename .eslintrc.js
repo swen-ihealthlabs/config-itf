c5168677777b10453d40734b7112714431e1f78a
swen <merlinwen@gmail.com>
2018-07-04 17:20:08 +0200

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,27 +3,26 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    sourceType: 'module'
   },
   env: {
-    browser: true,
+    browser: true
   },
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'prettier',
+    'prettier/standard',
+    'plugin:vue/essential'
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue', 'prettier'],
   // add your custom rules here
   rules: {
+    // https://github.com/doppelmutzi/eslint-prettier-vue-demo/blob/master/.eslintrc.js
+    'prettier/prettier': 'error',
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
-}
+};
